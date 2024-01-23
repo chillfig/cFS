d23131610d61c6d447d1bac04340b445d486946f
jdfiguer <jdfiguer@Linux.myguest.virtualbox.org>
2024-01-23 16:22:34 -0500

    Adds submodule manual remote updating

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+cfs_apps=("$@") # This will create an array from the arguments
+
 # Name of the remote to add
 remote_name="nasa"
 remote_url="https://github.com/nasa/cFS.git"
+
+# Store the current directory as the root directory
+export ROOT_DIR=$(pwd)
 
 # Check if the remote already exists
 if git remote | grep -qx "$remote_name"; then
@@ -13,9 +18,19 @@ else
     echo "Remote '$remote_name' added."
 fi
 
+# update parent module
 git fetch nasa
 git merge nasa/main
 git rebase nasa/main
 
 # update submodules
 git submodule update --init --recursive
+
+# Update submodules
+for app in "${cfs_apps[@]}"; do
+    cd "${app}"
+    git fetch
+    git checkout main
+    git pull
+    cd "$ROOT_DIR"
+done
