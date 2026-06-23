25ddeae1a5514013eef7868122e62ac0886c7238
Justin Figueroa <justin.d.figueroa@nasa.gov>
2026-06-23 01:06:33 +0000

    Adds clang-format-19 upgrade

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -2,19 +2,17 @@
 
 app=$1
 
-cd apps/$app
+REPO_ROOT="$(git -C "$(dirname "$(realpath "$0")")" rev-parse --show-toplevel)"
+APP_DIR="$REPO_ROOT/apps/$app"
 
-THIS_PATH="$(realpath "$0")"
-THIS_DIR="$(dirname "$THIS_PATH")"
-
-# Find all files in THIS_DIR which end in .ino, .cpp, etc., as specified
+# Find all files in APP_DIR which end in .ino, .cpp, etc., as specified
 # in the regular expression just below
-FILE_LIST="$(find "$THIS_DIR" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
+FILE_LIST="$(find "$APP_DIR" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
 
 echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
 
 # Format each file.
-# - NB: do NOT put quotes around `$FILE_LIST` below or else the `clang-format` command will 
-#   mistakenly see the entire blob of newline-separated file names as a SINGLE file name instead 
+# - NB: do NOT put quotes around `$FILE_LIST` below or else the `clang-format` command will
+#   mistakenly see the entire blob of newline-separated file names as a SINGLE file name instead
 #   of as a new-line separated list of *many* file names!
-clang-format-10 --verbose -i --style=file $FILE_LIST
+clang-format-19 --verbose -i --style=file:"$REPO_ROOT/.clang-format" $FILE_LIST
